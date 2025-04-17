a8fa263c3ade59ad71d143813ee5a0ea240f1d83
Chiara Marie Kappe <ckappe@3-E-20.42heilbronn.de>
2025-04-17 15:52:07 +0200

    fixed norm errors in ft_split

--- utils/ft_split.c
+++ utils/ft_split.c
@@ -6,13 +6,11 @@
 /*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 17:23:41 by ckappe            #+#    #+#             */
-/*   Updated: 2025/04/16 18:56:02 by ckappe           ###   ########.fr       */
+/*   Updated: 2025/04/17 14:48:26 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
-
-
 
 static int	ft_countstrs(const char *s, char c)
 {
@@ -102,101 +100,3 @@ char	**ft_split(char const *s, char c)
 	}
 	return (res);
 }
-
-
-
-/* static int	count_words(const char *s, char c)
-{
-	int	count;
-	int	in_word;
-
-	count = 0;
-	in_word = 0;
-	while (*s)
-	{
-		if (*s != c && in_word == 0)
-		{
-			in_word = 1;
-			count++;
-		}
-		else if (*s == c)
-			in_word = 0;
-		s++;
-	}
-	return (count);
-}
-
-static char	*extract_word(const char *s, int start, int end)
-{
-	char	*word;
-	int		i;
-
-	word = malloc((end - start + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	i = 0;
-	while (start < end)
-	{
-		word[i] = s[start];
-		i++;
-		start++;
-	}
-	word[i] = '\0';
-	return (word);
-}
-
-static char	**allocate_result(const char *s, char c)
-{
-	int		word_count;
-	char	**res;
-
-	word_count = count_words(s, c);
-	res = malloc((word_count + 1) * sizeof(char *));
-	if (!res)
-		return (NULL);
-	return (res);
-}
-
-static int	process_word(const char *s, char c, char **res, int *j)
-{
-	int		start;
-	int		i;
-
-	start = -1;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] != c && start == -1)
-			start = i;
-		else if ((s[i] == c || s[i + 1] == '\0') && start != -1)
-		{
-			if (s[i] == c)
-				res[*j] = extract_word(s, start, i);
-			else
-				res[*j] = extract_word(s, start, i + 1);
-			if (!res[*j])
-				return (0);
-			(*j)++;
-			start = -1;
-		}
-		i++;
-	}
-	return (1);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**res;
-	int		j;
-
-	if (!s)
-		return (NULL);
-	res = allocate_result(s, c);
-	if (!res)
-		return (NULL);
-	j = 0;
-	if (!process_word(s, c, res, &j))
-		return (NULL);
-	res[j] = NULL;
-	return (res);
-} */
